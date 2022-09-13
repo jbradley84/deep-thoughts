bcf48e3b8c2c1cb6a5fdf0163ff858e75b76dacf
jbradley84 <utbradley84@yahoo.com>
2022-09-13 15:59:53 -0500

    wrote mutations for adding/updating thoughts/reactions/friends

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,25 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
    Query: {
+      // get logged in user data
+      me: async (parent, args, context) => {
+         if (context.user) {
+            const userData = await User.findOne({})
+               .select('-__v -password')
+               .populate('thoughts')
+               .populate('friends');
+
+            return userData;
+         }
+
+         throw new AuthenticationError('Not logged in');
+      },
+
       // get all thoughts
-      thoughts: async (parent, {username}) => {
+      thoughts: async (parent, { username }) => {
          const params = username ? { username } : {};
          return Thought.find(params).sort({ createdAt: -1 });
       },
@@ -16,18 +32,93 @@ const resolvers = {
       // get all users
       users: async () => {
          return User.find()
-         .select('-__v -password')
-         .populate('friends')
-         .populate('thoughts');
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts');
       },
 
       // get single user by username
       user: async (parent, { username }) => {
          return User.findOne({ username })
-         .select('-__v -password')
-         .populate('friends')
-         .populate('thoughts');
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts');
       }
+   },
+
+   Mutation: {
+      // create a new user
+      addUser: async (parent, args) => {
+         const user = await User.create(args);
+         const token = signToken(user);
+
+         return { token, user };
+      },
+
+      // user login
+      login: async (parent, { email, password }) => {
+         const user = await User.findOne({ email });
+
+         if (!user) {
+            throw new AuthenticationError('Incorrect credentials');
+         }
+
+         const correctPw = await user.isCorrectPassword(password);
+
+         if (!correctPw) {
+            throw new AuthenticationError('Incorrect credentials');
+         }
+
+         const token = signToken(user);
+         return { token, user };
+      },
+
+      // add a new thought
+      addThought: async (parent, args, context) => {
+         if (context.user) {
+            const thought = await Thought.create({ ...args, username: context.user.username });
+
+            await User.findByIdAndUpdate(
+               { _id: context.user._id },
+               { $push: { thoughts: thought._id } },
+               { new: true }
+            );
+
+            return thought;
+         }
+
+         throw new AuthenticationError('You need to be logged in!');
+      },
+
+      // add a new reaction to a thought
+      addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+         if (context.user) {
+            const updatedThought = await Thought.findOneAndUpdate(
+               { _id: thoughtId },
+               { $push: { reactions: { reactionBody, username: context.user.username } } },
+               { new: true, runValidators: true }
+            );
+
+            return updatedThought;
+         }
+
+         throw new AuthenticationError('You need to be logged in!');
+      },
+
+      // add a new friend to a user
+      addFriend: async (parent, { friendId }, context) => {
+         if (context.user) {
+           const updatedUser = await User.findOneAndUpdate(
+             { _id: context.user._id },
+             { $addToSet: { friends: friendId } },
+             { new: true }
+           ).populate('friends');
+       
+           return updatedUser;
+         }
+       
+         throw new AuthenticationError('You need to be logged in!');
+       }
    }
 };
 
