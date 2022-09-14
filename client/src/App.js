f5320674a5f6f073c0ab959037e2256d3f3aa476
jbradley84 <utbradley84@yahoo.com>
2022-09-14 09:29:48 -0500

    added root level package.json to manage mern stack structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import React from 'react';
 
 import Header from './components/Header';
@@ -5,16 +6,32 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+// establish link to GraphQL server at graphql enpoint
+const httpLink = createHttpLink({
+   uri: '/graphql',
+});
+
+// instantiate Apollo Client instance & create connection to API endpoint
+const client = new ApolloClient({
+   link: httpLink,
+   // instantiate new cache object
+   cache: new InMemoryCache(),
+});
+
 function App() {
-  return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
-      </div>
-      <Footer />
-    </div>
-  );
+   // return JSX code
+   return (
+      // enable application to interact with Apollo Client instance
+      <ApolloProvider client={client}>
+         <div className='flex-column justify-flex-start min-100-vh'>
+            <Header />
+            <div className='container'>
+               <Home />
+            </div>
+            <Footer />
+         </div>
+      </ApolloProvider>
+   );
 }
 
 export default App;
