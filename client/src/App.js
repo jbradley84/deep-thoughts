94c5daaf14633b06dbb0a56c0a08cba2625dc7d0
jbradley84 <utbradley84@yahoo.com>
2022-09-14 12:18:07 -0500

    updated SingleThought.js with useParams useQuery, added ReactionList component

--- client/src/App.js
+++ client/src/App.js
@@ -47,18 +47,18 @@ function App() {
                         path="/signup"
                         element={<Signup />}
                      />
+                     <Route path="/profile">
+                        <Route path=":username" element={<Profile />} />
+                        <Route path="" element={<Profile />} />
+                     </Route>
                      <Route
-                        path="/profile"
-                        element={<Profile />}
-                     />
-                     <Route
-                        path="/thought"
+                        path="/thought/:id"
                         element={<SingleThought />}
                      />
-                     
+
                      <Route
-                     path="*"
-                     element={<NoMatch />}
+                        path="*"
+                        element={<NoMatch />}
                      />
                   </Routes>
                </div>
